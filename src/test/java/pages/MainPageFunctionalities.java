4715f3e5e02588897613dc5887a16b34d6acc720
ainagulyarba <ainagulyarba@gmail.com>
2022-02-17 23:23:18 +0100

    v

--- src/test/java/pages/MainPageFunctionalities.java
+++ src/test/java/pages/MainPageFunctionalities.java
@@ -1,7 +1,9 @@
 package pages;
 
+import utilities.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
 
 public class MainPageFunctionalities {
 
@@ -13,4 +15,23 @@ public class MainPageFunctionalities {
 
     //Locators
 
+    @FindBy(partialLinkText = "Signup")
+    public WebElement signupButton;
+
+    @FindBy(xpath = "(//h2)[3]")
+    public WebElement signupText;
+
+    @FindBy(name= "name")
+    public WebElement nameBox;
+
+    @FindBy (xpath = "(//*[@name=\"email\"])[2]")
+    public WebElement emailBox;
+
+    @FindBy(xpath = "(//*[@type= 'submit'])[2]")
+    public WebElement accountCreatSignupButton;
+
+    @FindBy(xpath = "//b[contains (text(),'Enter Account Information')]")
+    public WebElement createAccountPageVerification;
 }
+
+
